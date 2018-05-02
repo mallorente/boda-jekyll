20ec0d8609df62e46cd0cab6e71176ccce604e08
mallorentec <mallorente@gmail.com>
2018-05-02 08:19:28 +0200

    cambios Abril 2018

--- public/js/fondo-save.js
+++ public/js/fondo-save.js
@@ -8,15 +8,34 @@ function fondorandom(){
  
      //document.write("<img src='" + urlString + "' id='saveTheDateIm' type='file'>");
 
+     console.log(navigator.platform);
+
+
+     var userAgent = window.navigator.userAgent;
+
+     if (userAgent.match(/iPad/i) || userAgent.match(/iPhone/i)) {
+      document.write("<img src='" + urlString + "' id='saveTheDateIm' width='75%' style='-webkit-transform: scaleY(1); transform: scaleY(1);>'"); 
+     }
+     else {
+      switch(randNum){
+        case 6:
+        case 10:
+        case 11:
+        document.write("<img src='" + urlString + "' id='saveTheDateIm' width='75%' style='-webkit-transform: scaleY(-1); transform: scaleY(-1); >'");
+        default:
+        document.write("<img src='" + urlString + "' id='saveTheDateIm' width='75%' style='-webkit-transform: scaleY(1); transform: scaleY(1);>'");
+      }
+     }
+/*
      switch(randNum){
        case 6:
        case 10:
        case 11:
-       document.write("<img src='" + urlString + "' id='saveTheDateIm' width='60%' style='-webkit-transform: scaleY(-1); transform: scaleY(-1); >'");
+       document.write("<img src='" + urlString + "' id='saveTheDateIm' width='75%' style='-webkit-transform: scaleY(-1); transform: scaleY(-1); >'");
        default:
-       document.write("<img src='" + urlString + "' id='saveTheDateIm' width='60%' style='-webkit-transform: scaleY(1); transform: scaleY(1);>'");
+       document.write("<img src='" + urlString + "' id='saveTheDateIm' width='75%' style='-webkit-transform: scaleY(1); transform: scaleY(1);>'");
      }              
-
+*/
     /* MyDiv1.style.backgroundImage=urlString;
     MyDiv1.style.backgroundSize = "cover"; */
     
